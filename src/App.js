5d2d2b4f381318b19f612ba8ce84f7f99f508e58
tapos-ctrl <taposchandro231@gmail.com>
2022-02-06 23:33:29 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+// import './App.css';
+import Test from './pages/Test';
+
+function App() {
+  return (
+    <>
+    <Test/>
+    </>
+  );
+}
+
+export default App;
